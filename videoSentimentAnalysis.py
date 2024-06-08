bb91db8001469782720f34d5ea4aa3f419d02af5
Daria Vesela <daria1vesela@gmail.com>
2024-06-08 20:31:31 +0100

    Added the .py file

---
+++ videoSentimentAnalysis.py
@@ -0,0 +1,19 @@
+# bda
+
+# 1. Manually retrieve 10-15 random video URLs from YouTube.
+# Save the URLs in a text file called video_urls.txt , where each URL should be stored on a
+# separate line.
+# Consider YouTube videos that are 2-3 minutes in duration.
+
+def retrieve_videos():
+    ...
+
+# 2. Develop a Python script to read the URLs.
+# Assuming you have the text file named video_urls.txt containing the URLs of YouTube videos,
+# load it in Python and extract the URLs using your preferred data structure
+
+def extract_URLs():
+    ...
+
+
+
